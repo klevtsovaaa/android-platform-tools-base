1b060e72a5184c9cfc4d5a38412cde3c5c82df18
Repeat Yang <repeat@pixnet.tw>
2014-10-31 12:02:45 +0800

    Add rebuild.sh
    Reference: http://zewaren.net/site/?q=node/125

---
+++ rebuild.sh
@@ -0,0 +1,2 @@
+/usr/local/bin/javac sdklib/src/main/java/com/android/sdklib/internal/repository/archives/ArchFilter.java -cp "sdklib/src/main/java:common/src/main/java"
+/usr/local/bin/javac common/src/main/java/com/android/SdkConstants.java
